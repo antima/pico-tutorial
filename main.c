7408d466473f9d24414ac9e06463b2dc46f950db
mar <gim.marcello@gmail.com>
2021-07-15 23:47:15 +0200

    costruzione dell'esempio base con pin input e output pwm

--- main.c
+++ main.c
@@ -1,24 +1,45 @@
-#include <pico/stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+typedef enum {
+	FADEIN,
+	FADEOUT
+} fade_type;
+
+void fade(bool fade_in, const uint gpio) {
+	uint ctr = 0;
+	
+	for(int i = 0; i < 255; i++) {
+		if(fade_in) {
+			pwm_set_gpio_level(gpio, ctr * ctr);
+		} else {
+			pwm_set_gpio_level(gpio, (255 - ctr) * (255 - ctr));
+		}
+		sleep_ms(10);
+	}
+}
 
 int main() {
-	uint pirinfo;
-	const uint PIR_PIN = 0;
-	const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-	const uint BLED_PIN = 15;
+	uint pir_data;
+	const uint led1 = 0;
+	const uint led2 = 1;
+	const uint pir_sensor = 15;
 
-	gpio_init(PIR_PIN);
-	gpio_init(LED_PIN);
-	gpio_init(BLED_PIN);
+	gpio_set_function(led1, GPIO_FUNC_PWM);
+	gpio_set_function(led2, GPIO_FUNC_PWM);
 
+	gpio_init(pir_sensor);
+	gpio_set_dir(pir_sensor, GPIO_IN);
 
-	gpio_set_dir(PIR_PIN, GPIO_IN);
-	gpio_set_dir(LED_PIN, GPIO_OUT);
-	gpio_set_dir(BLED_PIN, GPIO_OUT);
+	uint slice_id_zero = pwm_gpio_to_slice_num(led1);
 
-	for(;;) {
-		pirinfo = gpio_get(PIR_PIN);
-		gpio_put(LED_PIN, pirinfo);
-		gpio_put(BLED_PIN, pirinfo);
-		sleep_ms(50);
+	pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 1.f);
+    pwm_init(slice_id_zero, &config, true);
+
+	while(1) {
+		pir_data = gpio_get(pir_sensor);
+		fade(pir_data, led1);
+		fade(pir_data, led2);
 	}
 }
